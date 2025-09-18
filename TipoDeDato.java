00e7731f37ec657a2be486828069834d8e291dbe
huetecobosmiguel-cloud <huete.cobos.miguel@iescamas.es>
2025-09-18 19:24:19 +0200

    Add files via upload

---
+++ TipoDeDato.java
@@ -0,0 +1,28 @@
+package com;
+
+public class TipoDeDato {
+
+	public static void main(String[] args) {
+		// Este es un tipo de dato númerico, entero.
+		int numero = 100;
+		float numero_decimal = 1.1F;
+		double numero_decimal2 = 1.1;
+		boolean logico = false;
+		String palabra = "tomate";
+		char letra = 'd';
+		
+		/**
+		 * Varias
+		 * Líneas
+		 * de Comentarios
+		 */
+		
+		System.out.println("Número: "+ numero);
+		System.out.println("Número decimal 1: " + numero_decimal);
+		System.out.println("Número decimal 2: " + numero_decimal2);
+		System.out.println("Lógico: " + logico);
+		System.out.println("Palabra: " + palabra);
+		System.out.println("Letra: " + letra);
+	}
+
+}
